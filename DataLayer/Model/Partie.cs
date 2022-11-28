3ce9f9ea37e7beb6da737c8407fcae682b728567
JEROME SCHLUB <jerome.schlub@sully-group.fr>
2022-11-28 11:53:10 +0100

    ajout du DataLayer et de la création des classes

---
+++ DataLayer/Model/Partie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Model
+{
+    public class Partie
+    {
+        [Key]
+        public int Guid { get; set; }
+        public string GrillePartie { get; set; }
+
+        #region cle etrangere de la table "Tirage"
+        public int TirageId { get; set; }
+        public Tirage Tirage { get; set; }
+        #endregion
+    }
+}
---
+++ DataLayer/Model/Tirage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Model
+{
+    public class Tirage
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ResultatTirage { get; set; }
+        public DateTime DateHeureTirage { get; set; }
+        public int CagnotteTirage { get; set; }
+
+        #region Un tirage peut avoir plusieurs partie
+        public List<Partie> Partie { get; set; }
+        #endregion
+    }
+}
